0180d9d4a7d4f7e7d96b6afac1f4d9495dd0e80e
DanielJanowicz <102616987+DanielJanowicz@users.noreply.github.com>
2022-09-30 14:41:09 -0400

    Dash Commit
    wip

--- host.py
+++ host.py
@@ -19,4 +19,4 @@ def dashboard():
     return render_template('ptdash.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80) 
